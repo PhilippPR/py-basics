65c9a23d038563068565b35f64b9da43b5b1fdbc
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:50:06 +0200

    Create list.py

---
+++ list/list.py
@@ -0,0 +1,17 @@
+"""
+List can be used to store multiple values in a single variable.
+
+to create a list in Python u use [] brackets.
+
+Reminder:
+    - Lists indexes start at 0
+    - a List in a List is Called a Dictionary (dict)
+"""
+
+Alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+print(Alphabet[12]) # Prints the value of the index 12.
+
+Numbers = [9000, 12000, 13000, 15000, 8000]
+
+print(Numbers[3]) # Prints the value of the index 3
