861eca3710b235d3e7a1bbb14f6b3d7d105d5e63
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:49:56 +0200

    Create string.py

---
+++ str/string.py
@@ -0,0 +1,12 @@
+"""
+A String is a sequence of characters.
+Every Character can be in a String.
+A String can start with " and end with " OR ' and end with '.
+Strings can contain numbers, letters, and symbols.
+"""
+
+print("Hello World") # if u use The print() function with normal text u are using a String.
+
+print("Hello World" + "!") # u can expand a String with another string with "+".
+
+print('Hello "World"') # The output on the screen is >>>Hello "World"<<< becouse the String dosnt end with " it starts with ' and ends with '.
