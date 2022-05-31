9e80eb122eddcc97eb6ebba725e5f7869d8ceaa8
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:49:58 +0200

    Create range.py

---
+++ range/range.py
@@ -0,0 +1,20 @@
+"""
+With Range u can get a range of numbers.
+
+A Range returns u a list of numbers.
+
+For Example:
+range(1,10) returns a list of numbers from 1 to 10 (1,2,3,4,5,6,7,8,9,10)
+
+in this Range u can Print an index of the list.
+
+Reminder:
+    - Lists indexes start at 0
+    - A Range from 1,10 will end at index [9] because an index starts at 0.
+"""
+
+Numbers = range(1,31) # Returns a list of numbers from 1 to 30.
+
+for i in Numbers:
+    print(i)
+
