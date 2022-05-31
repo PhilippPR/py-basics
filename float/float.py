a350c2be78c6bda9dd851fa0c364fa4d427fb6a4
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:50:10 +0200

    Create float.py

---
+++ float/float.py
@@ -0,0 +1,22 @@
+"""
+A Float is a class that represents a "broken" Number.
+Float's are whole numbers with a  with a decimal point. Like: 1,9 / 9,001 / -3,12 / -4,2001
+
+Reminder: 
+    - An Float number without a decimal point is a whole number like an Integer.
+    - A Float number is not an Integer.
+    - in Python u user "." for decimal numbers. (not ",")
+"""
+
+# Every Print will be Printed without that the number will be rounded.
+print(float(12))
+print(float(12.5))
+print(float(-12))
+print(float(-12.4))
+print(float(5))
+print(float(5.12))
+print(float(-5)) 
+print(float(-5.6)) 
+
+
+# if u wannt a rounded number u can use the int() function.
