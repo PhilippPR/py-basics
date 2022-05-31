698cbcbaf4de8b2eca10965773095740f82ebb7f
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:49:48 +0200

    Create comments.py

---
+++ examples/comments.py
@@ -0,0 +1,26 @@
+
+
+
+# This is a Simple comment it makes the whole line a comment. It dosent run any code that is on the line.
+# A Simple comment starts with a "#" and goes to the end of the line.
+# Simple comments can be used to make a code easier to read. And to make yourself or others easier to understand your code.
+# Simple comments are mostly used to even cut out a line in a code. For Example:
+#print("dlroW olleH")
+print("Hello World")
+
+# Simple comments can even be used to cut a part of a line. The "#" cuts the line so the rest of the line is ignored. For Example:
+
+print("Hello World") # This is getting ignored.
+
+
+
+"""
+This is a block comment.
+
+it can go several lines.
+
+every line is indented.
+
+print("This will not work. Because it is inside the comment lines.")
+"""
+
