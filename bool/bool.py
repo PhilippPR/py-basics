a4ba0dcb33470ebb8b1f489c716b2923776e1b2c
PhilippPR <philipprampfel@gmail.com>
2022-05-31 21:49:45 +0200

    Create bool.py

---
+++ bool/bool.py
@@ -0,0 +1,21 @@
+
+"""
+Bool is a class that represents a boolean value.
+Bool can be True or False.
+The Bool value in Binary is 0 or 1 (0 = False, 1 = True)
+
+By changing the value "ME" to True or False, you can change the value of the Bool.
+In Python u have not to define the type of the value. True or False is enough.
+If u want to be sure it is a Bool, u can use the bool() function. Like:
+ME = bool(True) or ME = bool(False)
+"""
+
+ME = True
+
+if ME == True:
+    print("This is True")
+if ME == False:
+    print("This is False")
+
+
+    
